4b59c9db486d71d1f2d5d55241fd1eb99c5fcf00
MeisteReK <128723315+MeisteReK@users.noreply.github.com>
2024-06-13 17:32:55 +0200

    Update edit.blade.php

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -15,25 +15,50 @@
                             <form action="{{ route('products.update', $product->id) }}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 @method('PUT')
+
                                 <div class="mb-3">
                                     <label for="name" class="form-label">Name:</label>
-                                    <input type="text" name="name" id="name" class="form-control" value="{{ $product->name }}" required>
+                                    <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $product->name) }}" required>
+                                    @error('name')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
+
                                 <div class="mb-3">
                                     <label for="description" class="form-label">Description:</label>
-                                    <textarea name="description" id="description" class="form-control" required>{{ $product->description }}</textarea>
+                                    <textarea name="description" id="description" class="form-control @error('description') is-invalid @enderror" required>{{ old('description', $product->description) }}</textarea>
+                                    @error('description')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
+
                                 <div class="mb-3">
                                     <label for="price" class="form-label">Price:</label>
-                                    <input type="text" name="price" id="price" class="form-control" value="{{ $product->price }}" required>
+                                    <input type="text" name="price" id="price" class="form-control @error('price') is-invalid @enderror" value="{{ old('price', $product->price) }}" required>
+                                    @error('price')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
+
                                 <div class="mb-3">
                                     <label for="image" class="form-label">Image:</label>
-                                    <input type="file" name="image" id="image" class="form-control">
+                                    <input type="file" name="image" id="image" class="form-control @error('image') is-invalid @enderror">
+                                    @error('image')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                     @if ($product->image)
                                         <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="img-thumbnail mt-2" width="150">
                                     @endif
                                 </div>
+
                                 <button type="submit" class="btn btn-primary">Update</button>
                             </form>
                         </div>
