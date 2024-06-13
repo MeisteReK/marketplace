89b4bedd92b7a4f469858bac4aa6ddde271c56b9
MeisteReK <128723315+MeisteReK@users.noreply.github.com>
2024-06-13 18:23:59 +0200

    Update 2024_05_23_134621_create_products_table.php

--- database/migrations/2024_05_23_134621_create_products_table.php
+++ database/migrations/2024_05_23_134621_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
-            $table->decimal('price', 8, 2);
+            $table->decimal('price', 12, 2);
             $table->string('image')->nullable();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
